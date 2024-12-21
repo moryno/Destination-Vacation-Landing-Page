75e13845a7bfff1df55b4b44874b0df349ff4929
moryno <mauricenganga41@gmail.com>
2024-12-21 18:00:50 +0300

    Add slides components

---
+++ components/Slides.tsx
@@ -0,0 +1,18 @@
+import { Data } from "@/app/page"
+import SliderCard from "./SliderCard"
+
+type Props = {
+    data: Data[]
+}
+
+const Slides = ({ data }: Props) => {
+  return (
+    <div className="flex w-full gap-6">
+      { data.map((data) => 
+        <SliderCard key={data.id} data={data} />
+      )}
+    </div>
+  )
+}
+
+export default Slides
